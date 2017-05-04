0679de19d89aa41a120d697cb73b0668a7538548
Anna <kanvalyariya@gmail.com>
2017-05-04 09:42:10 +0300

    Task7

--- JavaTests/src/test/java/SeleniumTests/Task7_Sidebar.java
+++ JavaTests/src/test/java/SeleniumTests/Task7_Sidebar.java
@@ -14,6 +14,9 @@ import java.util.List;
 import static org.openqa.selenium.By.*;
 
 public class Task7_Sidebar extends  TestBase {
+    String itemsMenu = "//ul[@id='box-apps-menu']/li";
+    String subItemsMenu = "//ul[@class='docs']/li";
+    String Title = "//main[@id='main']/h1";
     @Test
     public void MyFirstTest() {
         driver.get("http://localhost/litecart/admin/");
@@ -21,37 +24,24 @@ public class Task7_Sidebar extends  TestBase {
         driver.findElement(name("password")).sendKeys("admin");
         driver.findElement(name("remember_me")).click();
         driver.findElement(xpath(".//*[@id='box-login']/form/div[2]/button")).click();
-        //wait.until(ExpectedConditions.elementToBeClickable(id("sidebar")));
     }
 
     @Test
     public  void SidebarClick() {
-        String itemsMenu = "//ul[@id='box-apps-menu']/li";
-        String itemMenuLinkTemplate = "//ul[@id='box-apps-menu']/li[%s]/a";
-        String itemMenuTemplate = "//ul[@id='box-apps-menu']/li[%s]";
-        String subItemsMenu = "//ul[@class='docs']/li";
-        String subItemMenuTemplate = "//ul[@class='docs']/li[%s]";
-        String Title = "//main[@id='main']/h1";
-        int itemsCount = driver.findElements(By.xpath(itemsMenu)).size();
         List<WebElement> itemMenu = driver.findElements(By.xpath(itemsMenu));
         java.util.Iterator<WebElement> k = itemMenu.iterator();
         while(k.hasNext()) {
             WebElement item = k.next();
             item.click();
             System.out.println(item.getText());
-            List<WebElement> subItem = driver.findElements(By.xpath(subItemsMenu));
-            java.util.Iterator<WebElement> i = subItem.iterator();
-            while (i.hasNext()) {
-                WebElement subitems = i.next();
-                subitems.click();
-                System.out.println(subitems.getText());
             }
-        }
-
-
-
-
-
+        List<WebElement> subItem = driver.findElements(By.xpath(subItemsMenu));
+        java.util.Iterator<WebElement> i = subItem.iterator();
+        while (i.hasNext()) {
+            WebElement subitems = i.next();
+            subitems.click();
+            System.out.println(subitems.getText());
+            }
     }
 }
 
