ab5f1234297899e3599b49aaeed4f81157a6e7c5
Anna <kanvalyariya@gmail.com>
2017-05-12 16:46:34 +0300

    Task9

---
+++ JavaTests/src/main/java/OnlineShop/Countries.java
@@ -0,0 +1,87 @@
+package OnlineShop;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Countries {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    private String country = "//*[@id='main']/form/table//td[5]/a";
+    private String countryPage = "//*[@id='main']/h1";
+    private String numberOfZones = "//main[@id='main']//td[6]";
+    private String countryRefTemplate = "//main[@id='main']//tr[%s]/td[5]/a";
+    private String zonesRef = "//main[@id='main']//td[3]/input";
+
+    public Countries(WebDriver driver)
+    {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
+    }
+    public boolean isCountryTab()
+    {
+        By CountryPage = By.xpath(countryPage);
+        //wait.until(ExpectedConditions.textToBePresentInElementLocated(CountryPage, "Countries"));
+        return driver.findElement(CountryPage).isDisplayed();
+    }
+    public Boolean ListOfCountries() {
+        By CountryLink = By.xpath(country);
+        List<WebElement> countries = driver.findElements(CountryLink);
+        return SortingByAlphabet(countries);
+    }
+    private Boolean SortingByAlphabet(List<WebElement> countries)
+    {
+        for (int i = 1; i < countries.size(); i++)
+        {
+            if (countries.get(i).getText().compareTo(countries.get(i+1).getText()) > 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public ArrayList<Integer> getCountriesWithZone()
+    {
+        ArrayList<Integer> countNumbersOfCountry = new ArrayList<Integer>();
+        By lNumberOfZones = By.xpath(numberOfZones);
+        List<WebElement> elements = driver.findElements(lNumberOfZones);
+        for(int i=0;i<elements.size();i++){
+            if(elements.get(i).getText().equals("0")){}else{
+                countNumbersOfCountry.add(i+1);}
+        }
+        return countNumbersOfCountry;
+    }
+
+    public void goToCountry(int x){
+        By lCountryRefTemplate = By.xpath(String.format(countryRefTemplate, "" + x));
+        driver.findElement(lCountryRefTemplate).click();
+    }
+    public boolean isEditCountry()
+    {
+        By lPageFlag = By.xpath(countryPage);
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(lPageFlag, "Edit Country"));
+        return driver.findElement(lPageFlag).isDisplayed();
+    }
+    public Boolean checkSortZones()
+    {
+        By ZoneLink = By.xpath(zonesRef);
+        List<WebElement> zones = driver.findElements(ZoneLink);
+        return checkZonesByAlphabet(zones);
+    }
+    private Boolean checkZonesByAlphabet(List<WebElement> elements)
+    {
+        for (int i = 1; i < elements.size(); i++)
+        {
+            if (elements.get(i - 1).getAttribute("value").compareTo(elements.get(i).getAttribute("value")) > 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ JavaTests/src/test/java/SeleniumTests/Task9_Sorting.java
@@ -0,0 +1,57 @@
+package SeleniumTests;
+
+import OnlineShop.Countries;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.By.name;
+import static org.openqa.selenium.By.xpath;
+
+public class Task9_Sorting extends TestBase {
+    protected WebDriver driver;
+    protected Countries countries;
+    @Before
+    public void beforeClass() {
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void CountrySorting()
+    {
+        countries = new Countries(driver);
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(name("username")).sendKeys("admin");
+        driver.findElement(name("password")).sendKeys("admin");
+        driver.findElement(name("remember_me")).click();
+        driver.findElement(xpath(".//*[@id='box-login']/form/div[2]/button")).click();
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+       // countries.isCountryTab();
+        Assert.assertTrue(countries.ListOfCountries());
+    }
+    @Test
+    public void CountryZoneSorting()
+    {
+        //Assert Assert = new Assert();
+        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+        countries.isCountryTab();
+        ArrayList<Integer> countriesWithZone = countries.getCountriesWithZone();
+        for (int i = 0; i < countriesWithZone.size(); i++)
+        {
+            countries.goToCountry(countriesWithZone.get(i));
+            countries.isEditCountry();
+            Assert.assertTrue(countries.checkSortZones());
+            driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+            countries.isCountryTab();
+        }
+        Assert.assertTrue(countries.checkSortZones());
+    }
+
+}
